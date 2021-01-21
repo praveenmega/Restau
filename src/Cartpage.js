dfc74476a8cf1ccbc884e0e4e39ce9bf85f3ee55
Praveen <praveenmeka06@gmail.com>
2021-01-21 11:17:32 +0530

    adding total price feature

--- src/Cartpage.js
+++ src/Cartpage.js
@@ -7,33 +7,41 @@ const Cartpage =({cart,removeFromCart,len}) => {
         <h1>Don't think just add to cart</h1>
     );
 
-    const renderForm =() => (
-        <div>
-        <h1>Feel The Real Taste</h1>
-        <Form>
-            <Form.Group controlId="formBasicName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-            <Form.Group controlId="formBasicAddress">
-                <Form.Label>Address</Form.Label>
-                <Form.Control as="textarea" rows={3}  placeholder="Enter address" />
-            </Form.Group>
-            <Form.Group controlId="formBasicPhone">
-                <Form.Label>Phone No</Form.Label>
-                <Form.Control type="text" placeholder="Enter Phone Number" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Checkout
-            </Button>
-        </Form>
-        </div>
-    );
-
+    const renderForm =() => {
+        let total=cart.reduce((tot,product) => {
+            return tot+product.price;
+        },0)
+        console.log(total)
+        return(
+            <div>
+            <div style={{margin:20}}>
+                <h3 style={{fontWeight:'bold'}}>Total : {total.toFixed(2)} Rs</h3>
+            </div>
+            <h1>Feel The Real Taste</h1>
+            <Form>
+                <Form.Group controlId="formBasicName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter name" />
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group controlId="formBasicAddress">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control as="textarea" rows={3}  placeholder="Enter address" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPhone">
+                    <Form.Label>Phone No</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Phone Number" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Checkout
+                </Button>
+            </Form>
+            </div>
+        );
+    }
     return(
         <>
             <main style={{marginTop:76}}>
